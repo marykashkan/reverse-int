0f65070270c84e93847390a4fc77ffa223d3a051
Maryia Kashkan <mary.kashkan@gmail.com>
2022-07-09 02:05:55 +0300

    feat: add a solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let absNumber = Math.abs(n);
+    let result = 0;
+
+    while (absNumber) {
+        result = (result * 10) + (absNumber % 10);
+        absNumber = Math.floor(absNumber / 10);
+    };
+
+    return result;  
 }
